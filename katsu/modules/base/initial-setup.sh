7cad2083a3efd0e6b64dc901813b31d990767371
madonuko <madonuko@outlook.com>
2025-03-04 02:32:21 +0800

    feat: touch /.unconfigured

--- katsu/modules/base/initial-setup.sh
+++ katsu/modules/base/initial-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
-# Set up initial setup, might be redundant idk
-if rpm -q gnome-initial-setup; then
-    mkdir -p /var/lib/gdm
-    touch /var/lib/gdm/run-initial-setup
-else 
-    systemctl enable initial-setup || true
-fi
+
+## Set up initial setup, might be redundant idk
+#if rpm -q gnome-initial-setup; then
+#    mkdir -p /var/lib/gdm
+#    touch /var/lib/gdm/run-initial-setup
+#else 
+#    systemctl enable initial-setup || true
+#fi
 
 # Set default target to graphical
 systemctl set-default graphical.target
+
+touchc /.unconfigured
