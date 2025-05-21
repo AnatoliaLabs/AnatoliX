77e3e4004908554e2ed8a78f93a71f58f7e24513
june-fish <git@june.fish>
2025-05-21 10:41:11 -0500

    oop chroot

--- katsu/modules/ports/asahi/set-efi-uuid.sh
+++ katsu/modules/ports/asahi/set-efi-uuid.sh
@@ -5,5 +5,5 @@ UUID="7E5D-2CF4"
 printf "\x${UUID:7:2}\x${UUID:5:2}\x${UUID:2:2}\x${UUID:0:2}" \
 | dd bs=1 seek=67 count=4 conv=notrunc of=/dev/loop0s1
 
-sed -e 's/UUID=.*\/boot\/efi/UUID=$UUID \/boot\/efi/' /etc/fstab | sudo tee /etc/fstab
-sudo systemctl daemon-reload
+sed -e 's/UUID=.*\/boot\/efi/UUID=7E5D-2CF4 \/boot\/efi/' /etc/fstab | sudo tee /etc/fstab
+sudo /usr/lib/systemd/system-generators/systemd-fstab-generator ''
