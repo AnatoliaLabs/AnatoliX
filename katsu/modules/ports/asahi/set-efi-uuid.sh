57f53b97a468f02b6768859c6850fa26bde02075
june-fish <git@june.fish>
2025-05-21 10:10:41 -0500

    not enough coffee...

--- katsu/modules/ports/asahi/set-efi-uuid.sh
+++ katsu/modules/ports/asahi/set-efi-uuid.sh
@@ -6,4 +6,4 @@ printf "\x${UUID:7:2}\x${UUID:5:2}\x${UUID:2:2}\x${UUID:0:2}" \
 | dd bs=1 seek=67 count=4 conv=notrunc of=/dev/loop0s1
 
 sed -e 's/UUID=.*\/boot\/efi/UUID=$UUID \/boot\/efi/' /etc/fstab | sudo tee /etc/fstab
-sudo systemctl reload-daemon
+sudo systemctl daemon-reload
