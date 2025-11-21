16171fa9f94183c662134f3452c801a467cec8c8
madomado <madonuko@outlook.com>
2025-11-21 11:22:36 +0800

    Update cleanup.sh

--- katsu/modules/base/cleanup.sh
+++ katsu/modules/base/cleanup.sh
@@ -12,14 +12,6 @@ EOF
 # Set default timezone to UTC
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-# if aarch64
-
-arch=$(uname -m)
-if [[ $arch == "aarch64" ]]; then
-cp -P /usr/share/uboot/rpi_arm64/u-boot.bin /boot/efi/rpi-u-boot.bin
-cp -P /usr/share/uboot/rpi_3/u-boot.bin /boot/efi/rpi3-u-boot.bin
-cp -P /usr/share/uboot/rpi_4/u-boot.bin /boot/efi/rpi4-u-boot.bin
-fi
 rm -f /var/lib/systemd/random-seed
 rm -f /etc/NetworkManager/system-connections/*.nmconnection
 
